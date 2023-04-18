026e56d69f5e1d0f7ac90bdc1e1836d308435150
Sebastian <fase22290@icloud.com>
2023-04-18 22:00:43 +0200

    add factory states to TodoFactory

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Date;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
@@ -20,5 +21,32 @@ class TodoFactory extends Factory
             'title' => fake()->sentence(5),
             'description' => fake()->sentence(15),
         ];
+    }
+
+    public function status(string $status)
+    {
+        return $this->state(function ($status) {
+            return [
+                'status' => $status
+            ];
+        });
+    }
+
+    public function priority(string $priority)
+    {
+        return $this->state(function ($priority) {
+            return [
+                'priority' => $priority
+            ];
+        });
+    }
+
+    public function due_date(Date $due_date)
+    {
+        return $this->state(function ($due_date) {
+            return [
+                'due_date' => $due_date
+            ];
+        });
     }
 }
