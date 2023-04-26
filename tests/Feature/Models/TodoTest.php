587950f357d274e226478cae63e00919d6c6f51b
Sebastian <fase22290@icloud.com>
2023-04-26 20:51:27 +0200

    add furter tests to TodoTest

--- tests/Feature/Models/TodoTest.php
+++ tests/Feature/Models/TodoTest.php
@@ -4,5 +4,23 @@ use App\Models\Todo;
 use App\Models\User;
 use Illuminate\Database\QueryException;
 
-it('should throw a query exeption')->expect(fn() => Todo::factory()->create())->throws(QueryException::class);
-it('should create a Todo')->expect(fn() => Todo::factory()->forUser()->create())->toBeInstanceOf(Todo::class);
+function createTodo(array $args = null){
+   return  Todo::factory()->forUser()->create($args);
+}
+
+function makeTodo(array $args = null){
+    return  Todo::factory()->forUser()->make($args);
+ }
+
+it('should throw a query exeption')->expect(fn() =>Todo::factory()->create())->throws(QueryException::class);
+it('should create an entry in todos table')->defer(fn() => createTodo() )->assertDatabaseCount('todos', 1);
+it('should write the title in todos table')->defer(fn() => createTodo(['title' => '__TITLE__']))->assertDatabaseHas('todos', ['title' =>'__TITLE__']);
+it('should write the description in todos table')->defer(fn() => createTodo(['description' => '__DESCRIPTION__']))->assertDatabaseHas('todos', ['description' =>'__DESCRIPTION__']);
+it('should write LOW as default priority')->defer(fn() => createTodo(['description' => '__DESCRIPTION__']))->assertDatabaseHas('todos', ['priority' =>'LOW']);
+it('should write TODO as default status')->defer(fn() => createTodo(['description' => '__DESCRIPTION__']))->assertDatabaseHas('todos', ['status' =>'TODO']);
+
+it('should return an instance of todo')->expect(fn() => makeTodo())->toBeInstanceOf(Todo::class);
+it('should return an instance of a user')->expect(fn() => makeTodo()->user)->toBeInstanceOf(User::class);
+it('should return the title')->expect(fn()=> makeTodo(['title' => '__TITLE__'])->title)->toBe('__TITLE__');
+it('should return the description')->expect(fn()=>makeTodo(['description' => '__DESCRIPTION__'])->description)->toBe('__DESCRIPTION__');
+it('should be a string')->expect(fn($property)=> makeTodo()->$property)->toBeString()->with(["title", "description"]);
